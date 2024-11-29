0d5b2a53198d248496c0e50b4fb99697d6b42a0a
Chhrisst <134807284+Chhrisst@users.noreply.github.com>
2024-11-29 02:26:07 -0500

    Update Deteccion_QR.py

--- Deteccion_QR.py
+++ Deteccion_QR.py
@@ -1,3 +1,6 @@
+#----------------------------------------
+#LIBRERIAS
+#----------------------------------------
 import cv2  # Importa la biblioteca OpenCV para procesamiento de imágenes
 import numpy as np  # Importa NumPy para operaciones numéricas
 import pyzbar.pyzbar as pyzbar  # Importa pyzbar para decodificar códigos QR
@@ -5,14 +8,20 @@ import urllib.request  # Importa urllib para realizar solicitudes HTTP
 import os  # Importa el módulo os para reiniciar el script
 import sys  # Importa sys para obtener el nombre del script
 
-# URL base del ESP32 donde se encuentra la cámara
-url_base = 'http://192.168.XX.XX/'  # Cambia la IP según lo que el Serial Monitor de Arduino IDE arroje
+#----------------------------------------
+#CONFIGURACION INICIAL DE LA IP, VENTANA Y VARIABLES
+#----------------------------------------
+# URL base del ESP32-CAM
+url_base = 'http://192.168.18.177/'  # Cambia esto según tu configuración de red
 font = cv2.FONT_HERSHEY_PLAIN  # Define la fuente para el texto en la imagen
 cv2.namedWindow("Transmision del ESP32 CAM", cv2.WINDOW_AUTOSIZE)  # Crea una ventana para mostrar la transmisión en vivo
 
 prev = ""  # Variable para almacenar el último dato QR procesado
 pres = ""  # Variable para almacenar el dato QR actual
 
+#----------------------------------------
+#BUCLE PRINCIPAL DE CAPTURA Y PROCESAMIENTO DE IMAGENES DEL ESP32-CAM
+#----------------------------------------
 while True:  # Bucle principal para la captura de imágenes
     # Solicita la imagen de la cámara ESP32
     img_resp = urllib.request.urlopen(url_base + 'cam-hi.jpg')
@@ -20,6 +29,11 @@ while True:  # Bucle principal para la captura de imágenes
     imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
     # Decodifica la imagen en formato adecuado para OpenCV
     frame = cv2.imdecode(imgnp, -1)
+
+    # Redimensiona el fotograma para adaptarlo a la ventana
+    width = 240  # Ancho deseado
+    height = 240  # Alto deseado
+    frame = cv2.resize(frame, (width, height))
 
     # Decodifica los objetos QR presentes en la imagen
     decodedObjects = pyzbar.decode(frame)
@@ -31,7 +45,7 @@ while True:  # Bucle principal para la captura de imágenes
             prev = pres  # Actualiza el último dato procesado
             
             # Envía el dato QR al ESP32 a través de una solicitud HTTP
-            url_send = url_base + 'qr-data?data=' + urllib.parse.quote(pres)  # Codifica el dato para la URL
+            url_send = url_base + 'qr?data=' + urllib.parse.quote(pres)  # Codifica el dato para la URL
             urllib.request.urlopen(url_send)  # Realiza la solicitud para enviar el dato
 
             # Verifica si se detecta "EMERGENCIA"
@@ -42,11 +56,13 @@ while True:  # Bucle principal para la captura de imágenes
         # Muestra el dato QR en la imagen
         cv2.putText(frame, str(obj.data), (50, 50), font, 2, (255, 0, 0), 3)
 
-    # Muestra la imagen en la ventana creada
+    # Muestra la imagen redimensionada en la ventana creada
     cv2.imshow("Transmision del ESP32 CAM", frame)
 
+    #----------------------------------------
+    #TECLA ESPECIAL PARA CERRAR LA TRANSMISION
+    #----------------------------------------
     key = cv2.waitKey(1)  # Espera 1 milisegundo para detectar una tecla
     if key == ord('q'):  # Si se presiona 'q', sale del bucle
         break
-
 cv2.destroyAllWindows()  # Cierra todas las ventanas al finalizar
